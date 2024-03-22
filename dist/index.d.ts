ea053ca0da2851b17518f5c984a30835f0dd0861
Koti Royal <kotiroyal1999@gmail.com>
2024-03-22 15:23:26 +0530

    dist file created for basictable

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,10 +1,13 @@
-import React, { FC, ReactNode } from 'react';
+import * as React from 'react';
+import React__default, { FC, ReactNode } from 'react';
 
 interface Props {
     children: ReactNode | ReactNode[];
 }
 declare const Button: FC<Props>;
 
-declare const Typography: () => React.JSX.Element;
+declare const Typography: () => React__default.JSX.Element;
 
-export { Button, Typography };
+declare function BasicTable(): React.JSX.Element;
+
+export { BasicTable, Button, Typography };
